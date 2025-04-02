68afd1735052537fd1883ab797e320e1165e0212
liamsaul <lsaul95@gmail.com>
2025-04-02 12:44:29 +0100

    pelase work

--- scripts/install_npm.sh
+++ scripts/install_npm.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 set -e
 
-# Load NVM
-export NVM_DIR="$HOME/.nvm"
+# Load NVM for the ec2-user
+export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Install Node.js 23
+echo "Installing Node.js 23..."
+nvm install 23
+nvm use 23
+nvm alias default 23
+
+# Verify node is available
+node -v
+npm -v
 
 # Install npm dependencies
 cd /home/ec2-user/myapp
 npm install
+
+# Install nodemon globally to fix the path issue
+npm install -g nodemon
+
+# Fix permissions
+chmod -R 755 /home/ec2-user/myapp/node_modules
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,9 +2,18 @@
 set -e
 
 # Load NVM
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Use the correct Node version
+nvm use 23
 
 # Start the server
 cd /home/ec2-user/myapp
+echo "Starting server in directory: $(pwd)"
+echo "Node version: $(node -v)"
+echo "Using nodemon from: $(which nodemon)"
+
+# Start the application
 npm run start
