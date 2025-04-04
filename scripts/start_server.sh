9c318eb3bb23cf6c5e001d96eccdef761b14ec92
liamsaul <lsaul95@gmail.com>
2025-04-04 10:32:29 +0100

    please

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -62,6 +62,8 @@ echo "Starting application..."
 
 #!/bin/bash
 cd /home/ec2-user/myapp
+curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
+sudo yum install -y nodejs
 npm install
 pm2 start npm -- start
 pm2 save
